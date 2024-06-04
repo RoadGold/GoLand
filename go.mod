dcf2b72929d201e7d8cc151254449c24a3ca4666
Gonza <ronaldx2001020013@gmail.com>
2024-06-04 17:26:18 -0600

    updates

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module github.com/RoadGold/GoLand
 
 go 1.22.3
---
+++ variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var myIntVar int = -12
+	fmt.Println(myIntVar)
+
+	var myUintVar uint = 12
+	fmt.Println(myUintVar)
+
+	var myStryng string = "Esto es una cadena de texto"
+	fmt.Println(myStryng)
+
+	var myBoolVar bool = true
+	fmt.Println(myBoolVar)
+	fmt.Println(&myBoolVar)
+
+}
